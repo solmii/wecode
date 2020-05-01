9688d7bafeb5b94d097b232300e4515009ae14ce
solmii <rkdthfal4759@naver.com>
2020-05-01 22:11:00 +0900

    숫자 야구 게임 : 1. 정답 체크 2. 스트라이크, 볼 체크 후 힌트 출력 3. 정답 기회 10번 초과 시 GameOver

--- game.js
+++ game.js
@@ -1,33 +1,63 @@
 "use strict";
 
-// HTML 화면에 접근이 가능해짐
-
-//HTML에서 원하는 것을 JS로 불러온다.
-const body = document.body,
-  gameHint = document.querySelector(".js-hint"),
+//HTML에서 원하는 것을 JS로 불러오기
+const gameHint = document.querySelector(".js-hint"),
   gameForm = document.querySelector(".js-game"),
   gameInput = gameForm.querySelector("input"),
-  gameButton = document.querySelector(".js-gameButtom");
+  gameButton = document.querySelector(".js-gameButtom"),
+  logList = document.querySelector(".logList");
 
 // 1~9까지의 중복되지 않는 숫자 랜덤 생성
 const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const correctNumber = [];
+const answer = [];
 
-for (let i = 0; i < 4; i += 1) {
+while (answer.length < 4) {
   const randomNumber = numberList.splice(
     Math.floor(Math.random() * numberList.length),
     1
   )[0];
-  correctNumber.push(randomNumber);
+  answer.push(randomNumber);
 }
 
-// 정답 숫자 4개 미만/초과 입력시 안내창 출력
+// 중복 체크
 
 // 정답 체크
+let count = 0;
 
-// 스트라이크 체크
+gameForm.addEventListener("submit", function (event) {
+  // 도전! 버튼을 클릭했을때, 비동기 함수 실행
+  event.preventDefault();
+  const guess = gameInput.value;
 
-// 볼 체크
+  if (guess === answer.join("")) {
+    // 정답일 경우
+    gameHint.textContent = "Homerun!!";
+  } else {
+    // 오답일 경우
+    let guessList = guess.split("");
+    let strike = 0;
+    let ball = 0;
+    count += 1;
+    if (count > 10) {
+      // 기회 10번 초과시 Game Over
+      gameHint.textContent = `Gmae Over!! 정답은 ${answer}!`;
+    } else {
+      for (let i = 0; i < 3; i += 1) {
+        if (Number(guessList[i]) === answer[i]) {
+          //스트라이크 체크
+          strike += 1;
+        } else if (answer.indexOf(Number(guessList[i])) > -1) {
+          //볼 체크
+          ball += 1;
+        }
+      }
+      gameHint.textContent = `${strike}S ${ball}B`;
+      gameInput.value = "";
+    }
+  }
+});
+
+// 시도 횟수와 스트라이크, 볼 log 출력
 
 // 아웃 체크
 
